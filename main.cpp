4a7400212c02328868e47a74edab69de5819fb7d
Gabriel Lima <gabriel.lima.095@ufrn.edu.br>
2022-08-18 09:47:46 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,283 @@
+﻿#define _USE_MATH_DEFINES // for C++
+#define ARMA_USE_CXX11
+
+#include <cstdlib>
+#include <math.h>
+#include <random>
+#include <cmath>
+#include <iostream>
+#include <time.h>
+#include <fstream>
+#include <stdio.h>
+#include <chrono>
+#include <armadillo>
+using namespace std;
+using namespace arma;
+//#include "normal_rhythm.h"
+//#include "atrial_flutter.h"
+//#include "atrial_fibrillation.h"
+#include "ventricular_flutter.h"
+//#include "ventricular_fibrillation_wStim.h"
+//#include "ventricular_fibrillation_wouStim.h"
+
+double sinal(double x);
+
+int main(int argc, char** argv)
+{
+
+	errno_t saida, sw;
+	FILE* saida1, *saidaw;
+    saida = fopen_s(&saida1, "results.txt", "w");
+	if (!saida1)
+	{
+		perror("File opening failed");
+		return EXIT_FAILURE;
+	}
+	sw = fopen_s(&saidaw, "weights.txt", "w");
+	if (!saidaw)
+	{
+		perror("File opening failed");
+		return EXIT_FAILURE;
+	}
+
+	double t = 0.0;
+	double tf = 399.99;
+	double csr = 100.0;
+	double cst = 1.0 / csr;
+	double ssr = 1000.0;
+	double sst = 1.0 / ssr;
+	double asr = 10.0;
+	double ast = 0.0;
+	double stf, atf = 0.0;
+
+	arma::vec Q(3), Qp(3), Qpp(3, fill::zeros), Q_old(3);
+	arma::vec Q_delay(6);
+
+	double ECG, ECGp, ECGpp;
+
+	Q(0) = -0.1;
+	Qp(0) = 0.025;
+	Q(1) = -0.6;
+	Qp(1) = 0.1;
+	Q(2) = -3.3;
+	Qp(2) = 10.0/15.0;
+	Q_old.fill(0.0);
+
+	ECG = 1.0 + 0.06 * Q(0) + 0.1 * Q(1) + 0.3 * Q(2);
+	ECGp = 0.06 * Qp(0) + 0.1 * Qp(1) + 0.3 * Qp(2);
+
+	xt1_SA_AV_old = Q(0);
+	xt1_SA_HP_old = Q(0);
+	xt3_AV_SA_old = Q(1);
+	xt3_AV_HP_old = Q(1);
+	xt5_HP_SA_old = Q(2);
+	xt5_HP_AV_old = Q(2);
+
+	Q1d_SA_HP.set_size(tau_SA_HP / sst); Q1d_SA_HP.fill(0.0);
+	Q1d_SA_AV.set_size(tau_SA_AV / sst); Q1d_SA_AV.fill(0.0);
+	Q3d_AV_HP.set_size(tau_AV_HP / sst); Q3d_AV_HP.fill(0.0);
+	Q3d_AV_SA.set_size(tau_AV_SA / sst); Q3d_AV_HP.fill(0.0);
+	Q5d_HP_AV.set_size(tau_HP_AV / sst); Q5d_HP_AV.fill(0.0);
+	Q5d_HP_SA.set_size(tau_HP_SA / sst); Q5d_HP_SA.fill(0.0);
+	delay = { tau_AV_SA, tau_HP_SA, tau_SA_AV, tau_HP_AV, tau_SA_HP, tau_AV_HP };
+	delay_size = { tau_AV_SA / sst, tau_HP_SA / sst, tau_SA_AV / sst, tau_HP_AV / sst, tau_SA_HP / sst, tau_AV_HP / sst };
+
+	cout << delay_size(2) << endl;
+
+	ifstream file_in("upo_csr100.txt");
+	if (!file_in.is_open()) {
+		cout << "Error!\n";
+		return 0;
+	}
+
+	double u = 0.0, xd, xpd, xppd, e, ep, s;
+	double lambda = 10.0;
+	double d_est = 0.0;
+	double u_max = 3000.0;
+	double kappa = 15.0;
+	double b_est = 1.0;
+
+	double learn = 100.0;
+	int contador_max = 500000000;
+	int contador = 1;
+	double cam = 20.0;
+	int n = 6;
+	vec weight(n), width(n), center(n), V_ativ(n);
+	weight.fill(0.0);
+	center = { -cam / 2.0, -cam / 8.0, -cam / 16.0, cam / 16.0, cam / 8.0, cam / 2.0 };
+	width = { cam / 2.0, cam / 3.0, cam / 6.0, cam / 6.0, cam / 3.0, cam / 2.0 };
+	double lim_weight = 3000.0, lim_s = 1.0;
+	int training_opt = 0;
+
+	while (t <= tf){
+
+		file_in >> xd;
+		file_in >> xpd;
+		file_in >> xppd;
+
+		e = ECG - xd;
+		ep = ECGp - xpd;
+
+		s = ep + lambda * e;
+
+		if (t * 0.1048 < 16.0) {
+			u = 0.0;
+		}
+		else {
+			ativ(s, center, width, n, V_ativ);
+			atual(t, t + cst, weight, center, width, learn, s, contador_max, &contador, V_ativ, lim_weight, lim_s, training_opt);
+			d_est = dot(V_ativ, weight);
+		
+			u = (xppd - 2.0 * lambda * ep - lambda * lambda * e - d_est);
+		}
+
+		if (fabs(u) > u_max) { u = u_max * sinal(u); }
+
+		stf = t + cst;
+        while (t < stf) {
+			if (delay_size(0) < 1.0) {
+				Q_delay(0) = Q(1);
+			}
+			else {
+				if (t < delay(0)) {
+					Q_delay(0) = Q_old(1) - delay(0) * (Q(1) - Q_old(1)) / sst;
+					Q3d_AV_SA(cont_delay(0)) = Q_delay(0);
+					cont_delay(0) += 1.0;
+				}
+				else {
+					Q_delay(0) = Q3d_AV_SA(0);
+					for (int i = 0; i < cont_delay(0) - 1; i++) {
+						Q3d_AV_SA(i) = Q3d_AV_SA(i + 1);
+					}
+					Q3d_AV_SA(cont_delay(0) - 1) = Q(1);
+				}
+			}
+			
+			if (delay_size(1) < 1.0) {
+				Q_delay(1) = Q(2);
+			}
+			else {
+				if (t < delay(1)) {
+					Q_delay(1) = Q_old(2) - delay(1) * (Q(2) - Q_old(2)) / sst;;
+					Q5d_HP_SA(cont_delay(1)) = Q_delay(1);
+					cont_delay(1) += 1.0;
+				}
+				else {
+					Q_delay(1) = Q5d_HP_SA(0);
+					for (int i = 0; i < cont_delay(1) - 1; i++) {
+						Q5d_HP_SA(i) = Q5d_HP_SA(i + 1);
+					}
+					Q5d_HP_SA(cont_delay(1) - 1) = Q(2);
+				}
+			}
+			
+			if (delay_size(2) < 1.0) {
+				Q_delay(2) = Q(0);
+			}
+			else {
+				if (t < delay(2)) {
+					Q_delay(2) = Q_old(0) - delay(2) * (Q(0) - Q_old(0)) / sst;
+					Q1d_SA_AV(cont_delay(2)) = Q_delay(2);
+					cont_delay(2) += 1.0;
+				}
+				else {
+					Q_delay(2) = Q1d_SA_AV(0);
+					for (int i = 0; i < cont_delay(2) - 1; i++) {
+						Q1d_SA_AV(i) = Q1d_SA_AV(i + 1);
+					}
+					Q1d_SA_AV(cont_delay(2) - 1) = Q(0);
+				}
+			}
+
+			if (delay_size(3) < 1.0) {
+				Q_delay(3) = Q(2);
+			}
+			else {
+				if (t < delay(3)) {
+					Q_delay(3) = Q_old(2) - delay(3) * (Q(2) - Q_old(2)) / sst;
+					Q5d_HP_AV(cont_delay(3)) = Q_delay(3);
+					cont_delay(3) += 1.0;
+				}
+				else {
+					Q_delay(3) = Q5d_HP_AV(0);
+					for (int i = 0; i < cont_delay(3) - 1; i++) {
+						Q5d_HP_AV(i) = Q5d_HP_AV(i + 1);
+					}
+					Q5d_HP_AV(cont_delay(3) - 1) = Q(2);
+				}
+			}
+			
+			if (delay_size(4) < 1.0) {
+				Q_delay(4) = Q(0);
+			}
+			else {
+				if (t < delay(4)) {
+					Q_delay(4) = Q_old(0) - delay(4) * (Q(0) - Q_old(0)) / sst;
+					Q1d_SA_HP(cont_delay(4)) = Q_delay(4);
+					cont_delay(4) += 1.0;
+				}
+				else {
+					Q_delay(4) = Q1d_SA_HP(0);
+					for (int i = 0; i < cont_delay(4) - 1; i++) {
+						Q1d_SA_HP(i) = Q1d_SA_HP(i + 1);
+					}
+					Q1d_SA_HP(cont_delay(4) - 1) = Q(0);
+				}
+			}
+			
+			if (delay_size(5) < 1.0) {
+				Q_delay(5) = Q(1);
+			}
+			else {
+				if (t < delay(5)) {
+					Q_delay(5) = Q_old(1) - delay(5) * (Q(1) - Q_old(1)) / sst;
+					Q3d_AV_HP(cont_delay(5)) = Q_delay(5);
+					cont_delay(5) += 1.0;
+				}
+				else {
+					Q_delay(5) = Q3d_AV_HP(0);
+					for (int i = 0; i < cont_delay(5) - 1; i++) {
+						Q3d_AV_HP(i) = Q3d_AV_HP(i + 1);
+					}
+					Q3d_AV_HP(cont_delay(5) - 1) = Q(1);
+				}
+			}
+			Q_old = { Q(0), Q(1), Q(2) };
+            rk4(t, t + sst, Q, Qp, Q_delay, u);
+            t = t + sst;
+        }
+
+		edo(t, cst, Q, Qp, Q_delay, Qpp, u);
+		ECG = 1.0 + 0.06 * Q(0) + 0.1 * Q(1) + 0.3 * Q(2);
+		ECGp = 0.06 * Qp(0) + 0.1 * Qp(1) + 0.3 * Qp(2);
+		ECGpp = 0.06 * Qpp(0) + 0.1 * Qpp(1) + 0.3 * Qpp(2);
+
+        if(t>=atf && t * 0.1048 >= 14.0 && t * 0.1048 < 20.0){
+			fprintf(saida1, "%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t", t * 0.1048, ECG, ECGp, u, xd, xpd, e, ep, s, d_est, norm(weight, 2));
+			fprintf(saida1, "%.6f\t%.6f\t%.6f\t", Q(0), Q(1), Q(2));
+			fprintf(saida1, "%.6f\t%.6f\t%.6f\t", Qp(0), Qp(1), Qp(2));
+			fprintf(saida1, "\n"); 
+			fprintf(saidaw, "%.6f\t", t);
+			for (int i = 0; i < n; i++) {
+				fprintf(saidaw, "%.6f\t", weight(i));
+			}
+			fprintf(saidaw, "\n");
+        }
+
+	}
+
+	fclose(saida1);
+	fclose(saidaw);
+	file_in.close();
+
+	return 0;
+}
+
+double sinal(double x) {
+	if (fabs(x) > 0.0) {
+		return x / fabs(x);
+	}
+	else {
+		return 0.0;
+	}
+}
---
+++ ventricular_flutter.h
@@ -0,0 +1,206 @@
+#define M_PI 3.14159265358979323846
+
+double xt1_SA_AV_old = 0.0;
+double xt1_SA_HP_old = 0.0;
+double xt3_AV_SA_old = 0.0;
+double xt3_AV_HP_old = 0.0;
+double xt5_HP_SA_old = 0.0;
+double xt5_HP_AV_old = 0.0;
+
+double alp_SA = 3.0;
+double nu1_SA = 1.0;
+double nu2_SA = -1.9;
+double d_SA = 1.9;
+double e_SA = 0.55;
+
+double alp_AV = 3.0;
+double nu1_AV = 0.5;
+double nu2_AV = -0.5;
+double d_AV = 4.0;
+double e_AV = 0.67;
+
+double alp_HP = 7.0;
+double nu1_HP = 1.65;
+double nu2_HP = -2.0;
+double d_HP = 7.0;
+double e_HP = 0.67;
+
+double rho_SA = 0.0;
+double rho_HP = 0.0;
+double omega_SA = 0.0;
+double omega_HP = 0.0;
+double k_SA_AV = 3.0;
+double k_AV_HP = 45.0;
+double kt_SA_AV = 3.0;
+double kt_AV_HP = 20.0;
+double tau_SA_AV = 0.8;
+double tau_AV_HP = 0.1;
+double beta_t = 0.1111;
+
+double rho_AV = 0.0;
+double omega_AV = 0.0;
+double k_AV_SA = 0.0;
+double kt_AV_SA = 0.0;
+double k_HP_SA = 0.0;
+double kt_HP_SA = 0.0;
+double k_HP_AV = 0.0;
+double kt_HP_AV = 0.0;
+double k_SA_HP = 0.0;
+double kt_SA_HP = 0.0;
+double N_AV_HP = 1.0;
+double N_SA_AV = 1.0;
+double tau_SA_HP = 0.0;
+double tau_AV_SA = 0.0;
+double tau_HP_SA = 0.0;
+double tau_HP_AV = 0.0;
+
+arma::vec Q1d_SA_HP, Q1d_SA_AV, Q3d_AV_SA, Q3d_AV_HP, Q5d_HP_SA, Q5d_HP_AV;
+arma::vec delay(6), delay_size(6), cont_delay(6, fill::zeros);
+
+bool flag_SA_AV = true, flag_AV_HP = true;
+double xt1_SA_AV, xt3_AV_HP;
+
+double sgn(double x)
+{
+	if (x > 0.0)
+	{
+		return 1.0;
+	}else if (x < 0.0)
+	{
+		return -1.0;
+	}else{
+		return 0.0;
+	}
+}
+
+double sat(double x)
+{
+	if (x > 1.0)
+	{
+		return 1.0;
+	}
+	else if (x < -1.0)
+	{
+		return -1.0;
+	}
+	else
+	{
+		return x;
+	}
+
+}
+
+double FSA(double t) {
+	return rho_SA * sin(omega_SA * t);
+}
+double FAV(double t) {
+	return rho_AV * sin(omega_AV * t);
+}
+double FHP(double t) {
+	return rho_HP * sin(omega_HP * t);
+}
+
+void edo(double t, double dt, arma::vec Q, arma::vec Qp, arma::vec Q_delay, arma::vec& Qpp, double u) {
+	double x1, x2, x3, x4, x5, x6, x2p, x4p, x6p;
+
+	/*
+	0 - SA
+	1 - AV
+	2 - HP
+	*/
+
+	x1 = Q(0);
+	x2 = Qp(0);
+	x3 = Q(1);
+	x4 = Qp(1);
+	x5 = Q(2);
+	x6 = Qp(2);
+
+	x2p = FSA(t) - alp_SA * x2 * (x1 - nu1_SA) * (x1 - nu2_SA) - (x1 * (x1 + d_SA) * (x1 + e_SA)) / (d_SA * e_SA) - k_AV_SA * x1 + kt_AV_SA * Q_delay(0) - k_HP_SA * x1 + kt_HP_SA * Q_delay(1);
+	
+	x4p = FAV(t) - alp_AV * x4 * (x3 - nu1_AV) * (x3 - nu2_AV) - (x3 * (x3 + d_AV) * (x3 + e_AV)) / (d_AV * e_AV) - k_SA_AV * x3 + kt_SA_AV * Q_delay(2) - k_HP_AV * x3 + kt_HP_AV * Q_delay(3);
+
+	x6p = FHP(t) - alp_HP * x6 * (x5 - nu1_HP) * (x5 - nu2_HP) - (x5 * (x5 + d_HP) * (x5 + e_HP)) / (d_HP * e_HP) - k_SA_HP * x5 + kt_SA_HP * Q_delay(4) - k_AV_HP * x5 + kt_AV_HP * Q_delay(5) + u;
+
+	Qpp(0) = x2p;
+	Qpp(1) = x4p;
+	Qpp(2) = x6p;
+
+}
+
+void rk4(double t, double tf, arma::vec& Q, arma::vec& Qp, arma::vec Q_delay, double u){
+
+    arma::vec k1p(3), k2p(3), k3p(3), k4p(3), k1v(3), k2v(3), k3v(3), k4v(3);
+    arma::vec Qpp(3), k_delay(6);
+	double dt = tf - t;
+
+	/*
+	0 - SA
+	1 - AV
+	2 - HP
+	*/
+
+    edo(t, dt, Q, Qp, Q_delay, Qpp, u);
+    k1p = dt*Qp;
+    k1v = dt*Qpp;
+	k_delay = { k1p(1), k1p(2), k1p(0), k1p(2), k1p(0), k1p(1) };
+
+	edo(t + dt / 2.0, dt, Q + k1p / 2.0, Qp + k1v / 2.0, Q_delay + k_delay / 2.0, Qpp, u);
+    k2p = dt*(Qp + k1v/2.0);
+    k2v = dt*Qpp;
+	k_delay = { k2p(1), k2p(2), k2p(0), k2p(2), k2p(0), k2p(1) };
+
+    edo(t + dt/2.0, dt, Q + k2p/2.0, Qp + k2v/2.0, Q_delay + k_delay / 2.0, Qpp, u);
+    k3p = dt*(Qp + k2v/2.0);
+    k3v = dt*Qpp;
+	k_delay = { k3p(1), k3p(2), k3p(0), k3p(2), k3p(0), k3p(1) };
+
+    edo(t + dt, dt, Q + k3p, Qp + k3v, Q_delay + k_delay, Qpp, u);
+    k4p = dt*(Qp + k3v);
+    k4v = dt*Qpp;
+
+    Q = Q + (k1p + 2.0*k2p + 2.0 * k3p + k4p)/6.0;
+    Qp = Qp + (k1v + 2.0*k2v + 2.0 * k3v + k4v)/6.0;
+
+}
+
+void ativ(double x, arma::vec center, arma::vec width, int n, arma::vec& out)
+{
+
+	for (int i = 0; i < n; i++) {
+		out(i) = exp(-(x - center(i)) * (x - center(i)) / (2.0 * width(i) * width(i)));
+	}
+
+}
+
+void atual(double t, double tf, arma::vec& weight, arma::vec center, arma::vec width, double learn, double s, int contador_max, int* cont_ann, arma::vec d, double lim_weight, double lim_s, int training_opt) {
+
+	double h;
+	int contador;
+	h = tf - t;
+
+	contador = *cont_ann;
+
+	if (training_opt == 0) {//algoritmo de projecao
+		if (norm(weight, 2) < lim_weight || (norm(weight, 2) == lim_weight && learn * s * h * dot(weight, d) <= 0.0)) {
+			weight = weight + learn * s * h * d;
+		}
+		else {
+			weight = weight + (eye(6, 6) - (weight * weight.t()) / dot(weight, weight)) * learn * s * h * d;
+		}
+	}
+	else if (training_opt == 1) {//algoritmo da zona morta
+		if (fabs(s) >= lim_s) {
+			weight = weight + learn * s * h * d;
+		}
+	}
+	else {//tradicional
+		if (contador <= contador_max) {
+			weight = weight + learn * s * h * d;
+			contador++;
+		}
+	}
+
+	*cont_ann = contador;
+
+}
